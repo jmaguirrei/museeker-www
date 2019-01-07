a013e7d5e57b4619da21fecc4f0fa0eb3a218f23
José Manuel Aguirre <jmaguirrei@gmail.com>
2019-01-07 18:56:30 -0300

    Initial commit

--- public/app/sw.js
+++ public/app/sw.js
@@ -3,8 +3,8 @@
 
 const cacheName = 'v1';
 const cacheFiles = [
-  './app.js',
-  './index.html',
+  './base-www/app.js',
+  './base-www/index.html',
   'https://fonts.googleapis.com/css?family=Source+Sans+Pro',
 ];
 
