a85d961c433f2594148770c55a864d400ce24418
José Manuel Aguirre <jmaguirrei@gmail.com>
2019-01-21 11:37:14 -0300

    Initial commit

--- src/client/app.js
+++ src/client/app.js
@@ -1,4 +1,12 @@
-
 import config from '../config.js';
 
-export default window.jmaguirrei_client.init(config.client);
+let client;
+const isProduction = process.env.NODE_ENV === 'production';
+
+if (isProduction) {
+  client = window.jmaguirrei_client.init(config.client);
+} else {
+  client = require('@jmaguirrei/client').default.init(config.client);
+}
+
+export default client;
